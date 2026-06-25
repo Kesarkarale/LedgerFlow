b293d8a454072e8e6fb9dbddb73dfc61597ef4a3
Kesar Karale <kesarkarale1@gmail.com>
2026-06-25 19:00:18 +0530

    Update next-auth.d.ts

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,25 +1,23 @@
-import { DefaultSession } from "next-auth";
+import NextAuth, { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
   interface Session {
     user: {
       id: string;
-      email: string;
-      name: string;
     } & DefaultSession["user"];
   }
 
   interface User {
     id: string;
-    email: string;
-    name: string;
+    name?: string | null;
+    email?: string | null;
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
     id?: string;
-    email?: string | null;
     name?: string | null;
+    email?: string | null;
   }
 }
