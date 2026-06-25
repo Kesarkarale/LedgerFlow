a1d90ee7f02a34217d79762c3a5cca1415714bb2
Kesar Karale <kesarkarale1@gmail.com>
2026-06-25 18:22:34 +0530

    Update page.tsx

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,9 +1,5 @@
 import RegisterForm from "@/components/auth/register-form";
 
 export default function RegisterPage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-slate-50 px-4">
-      <RegisterForm />
-    </div>
-  );
+  return <RegisterForm />;
 }
